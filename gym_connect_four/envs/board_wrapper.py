e7c55286f8e0c7a3583c71bbd92a28248ffd6ad9
Thorben Jensen <jensen.thorben@gmail.com>
2020-02-20 21:15:11 +0100

    hotfix: no winner if board full without 'connect four'

--- gym_connect_four/envs/board_wrapper.py
+++ gym_connect_four/envs/board_wrapper.py
@@ -19,7 +19,7 @@ class ConnectFourWrapper(ConnectFourEnv):
         # make action
         out = super().step(action)
         # add info on who won to output
-        if (out[2] is True) and (self._winner == 0):
+        if (super().is_win_state() is True) and (self._winner == 0):
             self._winner = self.get_current_player()
         out[3]["winner"] = self._winner
         # change player and return output
@@ -29,15 +29,17 @@ class ConnectFourWrapper(ConnectFourEnv):
 
 c = ConnectFourWrapper(board_shape=(4, 4))
 
-c.step(2)
-c.step(1)
-c.step(3)
-c.step(0)
-c.step(2)
-c.step(1)
+for _ in range(3):
+    c.step(0)
+    c.step(1)
+    c.step(2)
+    c.step(3)
 
-c.available_moves()
+c.step(1)
+c.step(2)
 
+
+print(c.step(3))
 print(c.is_win_state())
-print(c.step(2))
+print(c.step(0))
 print(c.is_win_state())
